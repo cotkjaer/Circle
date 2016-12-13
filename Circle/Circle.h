ac4fca5a8b4113c7061f3809021651d021fe8fc7
christian otkjær <christian.otkjaer@gmail.com>
2016-12-13 10:58:25 +0100

    Initial Commit

---
+++ Circle/Circle.h
@@ -0,0 +1,19 @@
+//
+//  Circle.h
+//  Circle
+//
+//  Created by Christian Otkjær on 13/12/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Circle.
+FOUNDATION_EXPORT double CircleVersionNumber;
+
+//! Project version string for Circle.
+FOUNDATION_EXPORT const unsigned char CircleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Circle/PublicHeader.h>
+
+
